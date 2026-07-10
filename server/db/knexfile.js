f483e05cfc8449e2c8840d59da3ffdbe0f587c7d
Rebecca Ingulkar <rebeccaingulkar@outlook.com>
2026-07-10 13:56:58 +1200

    configured prior to making live on render

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -32,15 +32,29 @@ export default {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
     },
   },
-
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+    client: 'postgresql',
     connection: {
-      filename: '/app/storage/prod.sqlite3',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+      min: 2,
+      max: 10,
     },
   },
+  // production: {
+  //   client: 'sqlite3',
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: '/app/storage/prod.sqlite3',
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+  //   },
+  // },
 }
